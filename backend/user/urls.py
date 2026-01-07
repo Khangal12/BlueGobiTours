805525ac7e814709da5e4704d3f183bb1d1b58e8
oyu <enkhoyun0901@gmail.com>
2026-01-07 21:44:00 +0800

    update

--- backend/user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LoginAPIView, CheckTokenAPIView
-
-urlpatterns = [
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('token/', CheckTokenAPIView.as_view(), name='check_token'),
-]
